8a6bff6d03d49f57b6eea2920f76d876ad86775c
Patrick Breen <pbreen@uga.edu>
2017-02-06 16:56:28 -0500

    add naive supervised MLP based computer player

--- human_play_pong.py
+++ human_play_pong.py
@@ -1,30 +1,67 @@
 import sys
+import shutil
+import os
 
 import pygame
+import numpy as np
 
 # parameters
-size = width, height = (140, 100)
+size = width, height = (280, 200)
 wait = 10
 black = (0, 0, 0)
 velocity = [1, 1]
 goals_scored = 0
-frame_capture = 5
+frame_capture = 10
 
 # set up objects
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("ball.jpeg")
-ball = pygame.transform.scale(ball, (5, 5))
+ball = pygame.transform.scale(ball, (20, 20))
 ballrect = ball.get_rect()
-ballrect = ballrect.move([10, 10])
+ballrect = ballrect.move([20, 20])
 
 pad1 = pygame.image.load("paddle.jpeg")
-pad1 = pygame.transform.scale(pad1, (5, 14))
+pad1 = pygame.transform.scale(pad1, (15, 40))
 padrect1 = pad1.get_rect()
 padrect1 = padrect1.move([0, 70])
 
 pygame.font.init()
-myfont = pygame.font.SysFont("Comic Sans MS", 10)
+myfont = pygame.font.SysFont("Comic Sans MS", 16)
+
+# clear saved gameplay
+shutil.rmtree("saved_gameplay/actions", ignore_errors=True)
+os.mkdir("saved_gameplay/actions") 
+shutil.rmtree("saved_gameplay/screens", ignore_errors=True)
+os.mkdir("saved_gameplay/screens") 
+
+# convert hex string representation of screen to numpy array
+def get_img_mat_from_hex(screen):
+  screen = screen.strip()
+
+  red_ar = np.empty((width,height), dtype=np.float16)
+  green_ar = np.empty((width,height), dtype=np.float16)
+  blue_ar = np.empty((width,height), dtype=np.float16)
+
+  row = 0
+  col = 0
+
+  for start in range(0, len(screen), 8):
+    red_ar[row, col] = int(screen[start:start+2], 16)
+    green_ar[row, col] = int(screen[start+2:start+4], 16)
+    blue_ar[row, col] = int(screen[start+4:start+6], 16)
+
+    if row < width-1:
+      row += 1
+    else:
+      col += 1
+      row = 0
+      
+  red_ar = red_ar.T
+  green_ar = green_ar.T
+  blue_ar = blue_ar.T
+
+  return np.concatenate((red_ar[...,np.newaxis],green_ar[...,np.newaxis], blue_ar[...,np.newaxis]),axis=2)
 
 # game loop
 frame = 0
@@ -68,12 +105,12 @@ while True:
   keys = pygame.key.get_pressed()
   if keys[pygame.K_UP] and padrect1.top > 0:
     padrect1 = padrect1.move([0, -1])
-    action = 1
+    action = [1, 0, 0]
   elif keys[pygame.K_DOWN] and padrect1.bottom < height:
     padrect1 = padrect1.move([0, 1])
-    action = -1
+    action = [0, 1, 0]
   else:
-    action = 0
+    action = [0, 0, 1]
 
   # update score
   score_surface = myfont.render("goals scored = " + str(goals_scored), False, (255, 255, 255))
@@ -88,12 +125,12 @@ while True:
   # capture screen, action and score every "frame_capture" frames
   if frame % frame_capture == 0:
     screen_str = pygame.image.tostring(screen, "RGBX").hex()
-    with open("saved_gameplay/actions.txt", "a") as f:
-      f.write(str(action) + "\n")
-    with open("saved_gameplay/scores.txt", "a") as f:
+    np_array_action = np.array(action)
+    np.save("saved_gameplay/actions/" + str(frame), np_array_action)
+    with open("saved_gameplay/scores.txt", "w") as f:
       f.write(str(goals_scored) + "\n")
-    with open("saved_gameplay/screens.txt", "a") as f:
-      f.write(screen_str + "\n")
+    np_array_screen = get_img_mat_from_hex(screen_str)
+    np.save("saved_gameplay/screens/" + str(frame), np_array_screen)
 
   frame += 1
 
